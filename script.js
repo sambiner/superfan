f065fa49ea2a5f9147e109a3cfcc858c381e361b
Sam Biner <samuel_biner@student.davincischools.org>
2020-05-21 21:18:26 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var x = window.matchMedia("(max-width: 500px)");
 var one = document.getElementById("1");
-var two = document.getElementById("2"); //These are all the variables the appear in my JavaScript
+var two = document.getElementById("2");
 var three = document.getElementById("3");
 var four = document.getElementById("4");
 var five = document.getElementById("5");
@@ -20,42 +20,36 @@ var tensided = document.getElementById("10-sided");
 var randomnumb = document.getElementById("random-numb");
 var img = document.getElementById("img");
 var placealigner = document.getElementById("placealigner");
-var smallaligner = document.getElementById("smallaligner");
-var spacer = document.getElementById("spacer");
-var scream = document.getElementById("scream")
 var troll = document.getElementById("troll");
+var place1 = document.getElementById("place1");
+var place2 = document.getElementById("place2");
+var place3 = document.getElementById("place3");
+var place4 = document.getElementById("place4");
+var place5 = document.getElementById("place5");
+var place6 = document.getElementById("place6");
+var place7 = document.getElementById("place7");
+var place8 = document.getElementById("place8");
+var place9 = document.getElementById("place9");
+var place10 = document.getElementById("place10");
 var rcolumn = document.getElementById("rcolumn");
 var column1 = document.getElementById("column1");
-const element1 = document.getElementById("6-sided"); //These constants are to check if the class "opaque" is contained within the div in the Click# functions
+const element1 = document.getElementById("6-sided");
 const element2 = document.getElementById("10-sided");
-const element3 = document.getElementById("1");
-const element4 = document.getElementById("2");
-const element5 = document.getElementById("3");
-const element6 = document.getElementById("4");
-const element7 = document.getElementById("5");
-const element8 = document.getElementById("6");
-const element9 = document.getElementById("7");
-const element10 = document.getElementById("8");
-const element11 = document.getElementById("9");
-const element12 = document.getElementById("10");
 
 
-function myFunction(x){ //This makes the background image change and a sound play when the viewport width is smaller than 500px
+function myFunction(x){
 	if(x.matches){
 		alert("You have ANGERED Jonathan!");
-		scream.play();
 	} else {
-		scream.pause();
 	}
 }
 myFunction(x);
 x.addListener(myFunction);
-function optionsToggle(elem){ //This toggles the options page to open and close
+function optionsToggle(elem){
 	rcolumn.classList.toggle("display");
 	column1.classList.toggle("full-width");
-	spacer.classList.toggle("display");
 }
-function diceSizeToggle(elem){ //This toggles the dice sizes to appear and disappear
+function diceSizeToggle(elem){
 	sixsided.classList.toggle("display");
 	tensided.classList.toggle("display");
 	spacer1.classList.toggle("display");
@@ -64,7 +58,7 @@ function diceSizeToggle(elem){ //This toggles the dice sizes to appear and disap
 	spacer4.classList.toggle("display");
 	spacer5.classList.toggle("display");
 	spacer6.classList.toggle("display");
-	troll.classList.add("display");
+	troll.classList.toggle("display");
 	one.classList.add("display");
 	two.classList.add("display");
 	three.classList.add("display");
@@ -76,35 +70,25 @@ function diceSizeToggle(elem){ //This toggles the dice sizes to appear and disap
 	nine.classList.add("display");
 	ten.classList.add("display");
 }
-function trollMode(elem){ //This makes the numbers appear
-	if(element1.classList.contains("no-opaque")){
-		one.classList.remove("display");
-		two.classList.remove("display");
-		three.classList.remove("display");
-		four.classList.remove("display");
-		five.classList.remove("display");
-		six.classList.remove("display");
-		placealigner.classList.remove("display");
-		seven.classList.add("display");
-		eight.classList.add("display");
-		nine.classList.add("display");
-		ten.classList.add("display");
-	} else if(element2.classList.contains("no-opaque")){
-		one.classList.remove("display");
-		two.classList.remove("display");
-		three.classList.remove("display");
-		four.classList.remove("display");
-		five.classList.remove("display");
-		six.classList.remove("display");
-		seven.classList.remove("display");
-		eight.classList.remove("display");
-		nine.classList.remove("display");
-		ten.classList.remove("display");
-		smallaligner.classList.remove("display");
-		placealigner.classList.add("display");
+function displayToggle(){
+	one.classList.toggle("display");
+	two.classList.toggle("display");
+	three.classList.toggle("display");
+	four.classList.toggle("display");
+	five.classList.toggle("display");
+	six.classList.toggle("display");
+}
+function trollMode(elem){
+	if(element1.classList.contains("opaque")){
+		displayToggle();
+	} else if(element2.classList.contains("opaque")){
+		seven.classList.toggle("display");
+		eight.classList.toggle("display");
+		nine.classList.toggle("display");
+		ten.classList.toggle("display");
 	} else {}
 }
-function diceRoll(elem){ //Number randomizer and troll mode if statements
+function diceRoll(elem){
 	document.getElementById("random-numb").innerHTML = "Rolling the dice...";
 	document.getElementById("img").src = "images/dice-rolling2.gif";
 	if (element1.classList.contains("no-opaque") && element3.classList.contains("opaque")){
@@ -187,41 +171,15 @@ function diceRoll(elem){ //Number randomizer and troll mode if statements
 			randomnumb.innerHTML = "You rolled a 10";
 			img.src = "images/die-10.jpg";
 		}, 1500);
-	} else if(element1.classList.contains("no-opaque")){
+	} else if (element1.classList.contains("opaque")){
 		setTimeout(function (){
-			var roll = Math.floor(Math.random()*6) +1;
-			document.getElementById("random-numb").innerHTML = "You rolled a " + roll;
-			if (randomnumb.innerHTML == "You rolled a 1"){
-				img.src = "images/die-1.jpg";
-			} else if (randomnumb.innerHTML == "You rolled a 2"){
-				img.src = "images/die-2.jpg";
-			} else if (randomnumb.innerHTML == "You rolled a 3"){
-				img.src = "images/die-3.jpg";
-			} else if (randomnumb.innerHTML == "You rolled a 4"){
-				img.src = "images/die-4.PNG";
-			} else if (randomnumb.innerHTML == "You rolled a 5"){
-				img.src = "images/die-5.PNG";
-			} else if (randomnumb.innerHTML == "You rolled a 6"){
-				img.src = "images/die-6.png";
-			} else {}
+			defaultRandom(6);
+			defaultNumbers();
 		}, 1500);
-	} else if (element2.classList.contains("no-opaque")){
+	} else if (element2.classList.contains("opaque")){
 		setTimeout(function (){
-			var roll = Math.floor(Math.random()*10) +1;
-			document.getElementById("random-numb").innerHTML = "You rolled a " + roll;
-			if (randomnumb.innerHTML == "You rolled a 1"){
-				img.src = "images/die-1.jpg";
-			} else if (randomnumb.innerHTML == "You rolled a 2"){
-				img.src = "images/die-2.jpg";
-			} else if (randomnumb.innerHTML == "You rolled a 3"){
-				img.src = "images/die-3.jpg";
-			} else if (randomnumb.innerHTML == "You rolled a 4"){
-				img.src = "images/die-4.PNG";
-			} else if (randomnumb.innerHTML == "You rolled a 5"){
-				img.src = "images/die-5.PNG";
-			} else if (randomnumb.innerHTML == "You rolled a 6"){
-				img.src = "images/die-6.png";
-			} else if (randomnumb.innerHTML == "You rolled a 7"){
+			defaultRandom(10);
+			if (randomnumb.innerHTML == "You rolled a 7"){
 				img.src = "images/die-7.jpg";
 			} else if (randomnumb.innerHTML == "You rolled a 8"){
 				img.src = "images/die-8.jpg";
@@ -229,60 +187,55 @@ function diceRoll(elem){ //Number randomizer and troll mode if statements
 				img.src = "images/die-9.jpg";
 			} else if (randomnumb.innerHTML == "You rolled a 10"){
 				img.src = "images/die-10.jpg";
-			} else {}
+			} else {defaultNumbers();}
 		}, 1500);
 	} else {
 		setTimeout(function (){
-			var roll = Math.floor(Math.random()*6) +1;
-			document.getElementById("random-numb").innerHTML = "You rolled a " + roll;
-			if (randomnumb.innerHTML == "You rolled a 1"){
-				img.src = "images/die-1.jpg";
-			} else if (randomnumb.innerHTML == "You rolled a 2"){
-				img.src = "images/die-2.jpg";
-			} else if (randomnumb.innerHTML == "You rolled a 3"){
-				img.src = "images/die-3.jpg";
-			} else if (randomnumb.innerHTML == "You rolled a 4"){
-				img.src = "images/die-4.PNG";
-			} else if (randomnumb.innerHTML == "You rolled a 5"){
-				img.src = "images/die-5.PNG";
-			} else if (randomnumb.innerHTML == "You rolled a 6"){
-				img.src = "images/die-6.png";
-			} else {}
+			defaultRandom(6);
+			defaultNumbers();
 		}, 1500);
 	}
 }
-function reset(elem){ //resets website
-	window.location.reload();
-}
-function opaqueSix(elem){ //When 6-sided is clicked, it turns opaque
-	if(element2.classList.contains("opaque")){
-		sixsided.classList.add("no-opaque");
-		sixsided.classList.remove("opaque");
-		troll.classList.remove("display");
-	} else if(element2.classList.contains("no-opaque")){
-		sixsided.classList.add("no-opaque");
-		sixsided.classList.remove("opaque");
-		tensided.classList.remove("no-opaque");
-		tensided.classList.add("opaque");
-		troll.classList.remove("display");
+function defaultNumbers(){
+	if (randomnumb.innerHTML == "You rolled a 1"){
+		img.src = "images/die-1.jpg";
+	} else if (randomnumb.innerHTML == "You rolled a 2"){
+		img.src = "images/die-2.jpg";
+	} else if (randomnumb.innerHTML == "You rolled a 3"){
+		img.src = "images/die-3.jpg";
+	} else if (randomnumb.innerHTML == "You rolled a 4"){
+		img.src = "images/die-4.PNG";
+	} else if (randomnumb.innerHTML == "You rolled a 5"){
+		img.src = "images/die-5.PNG";
+	} else if (randomnumb.innerHTML == "You rolled a 6"){
+		img.src = "images/die-6.png";
 	} else {}
 }
-function opaqueTen(elem){ //When 10-sided is clicked, it turns opque
-	tensided.classList.add("no-opaque");
-	tensided.classList.remove("opaque");
-	sixsided.classList.add("opaque");
-	sixsided.classList.remove("no-opaque");
-	troll.classList.remove("display");
+function defaultRandom(x){
+	var roll = Math.floor(Math.random()*x) +1;
+	document.getElementById("random-numb").innerHTML = "You rolled a " + roll;
 }
-function clickOne(elem){ //These functions make Troll Mode work
-	if (element1.classList.contains("no-opaque")){
+function reset(elem){
+	randomnumb.innerHTML = "Random Number";
+	img.src = "images/dice-stationary.PNG";
+}
+function opaqueSix(elem){
+	sixsided.classList.add("opaque");
+	tensided.classList.remove("opaque");
+}
+function opaqueTen(elem){
+	tensided.classList.add("opaque");
+	sixsided.classList.remove("opaque");
+}
+function clickOne(elem){
+	if (element1.classList.contains("opaque")){
 		one.classList.add("opaque");
 		two.classList.remove("opaque");
 		three.classList.remove("opaque");
 		four.classList.remove("opaque");
 		five.classList.remove("opaque");
 		six.classList.remove("opaque");
-	} else if (element2.classList.contains("no-opaque")){
+	} else if (element2.classList.contains("opaque")){
 		one.classList.add("opaque");
 		two.classList.remove("opaque");
 		three.classList.remove("opaque");
@@ -296,14 +249,14 @@ function clickOne(elem){ //These functions make Troll Mode work
 	} else {}
 }
 function clickTwo(elem){
-	if (element1.classList.contains("no-opaque")){
+	if (element1.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.add("opaque");
 		three.classList.remove("opaque");
 		four.classList.remove("opaque");
 		five.classList.remove("opaque");
 		six.classList.remove("opaque");
-	} else if (element2.classList.contains("no-opaque")){
+	} else if (element2.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.add("opaque");
 		three.classList.remove("opaque");
@@ -317,14 +270,14 @@ function clickTwo(elem){
 	} else {}
 }
 function clickThree(elem){
-	if (element1.classList.contains("no-opaque")){
+	if (element1.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.remove("opaque");
 		three.classList.add("opaque");
 		four.classList.remove("opaque");
 		five.classList.remove("opaque");
 		six.classList.remove("opaque");
-	} else if (element2.classList.contains("no-opaque")){
+	} else if (element2.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.remove("opaque");
 		three.classList.add("opaque");
@@ -338,14 +291,14 @@ function clickThree(elem){
 	} else {}
 }
 function clickFour(elem){
-	if (element1.classList.contains("no-opaque")){
+	if (element1.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.remove("opaque");
 		three.classList.remove("opaque");
 		four.classList.add("opaque");
 		five.classList.remove("opaque");
 		six.classList.remove("opaque");
-	} else if (element2.classList.contains("no-opaque")){
+	} else if (element2.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.remove("opaque");
 		three.classList.remove("opaque");
@@ -359,14 +312,14 @@ function clickFour(elem){
 	} else {}
 }
 function clickFive(elem){
-	if (element1.classList.contains("no-opaque")){
+	if (element1.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.remove("opaque");
 		three.classList.remove("opaque");
 		four.classList.remove("opaque");
 		five.classList.add("opaque");
 		six.classList.remove("opaque");
-	} else if (element2.classList.contains("no-opaque")){
+	} else if (element2.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.remove("opaque");
 		three.classList.remove("opaque");
@@ -380,14 +333,14 @@ function clickFive(elem){
 	} else {}
 }
 function clickSix(elem){
-	if (element1.classList.contains("no-opaque")){
+	if (element1.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.remove("opaque");
 		three.classList.remove("opaque");
 		four.classList.remove("opaque");
 		five.classList.remove("opaque");
 		six.classList.add("opaque");
-	} else if (element2.classList.contains("no-opaque")){
+	} else if (element2.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.remove("opaque");
 		three.classList.remove("opaque");
@@ -401,7 +354,7 @@ function clickSix(elem){
 	} else {}
 }
 function clickSeven(elem){
-	if (element2.classList.contains("no-opaque")){
+	if (element2.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.remove("opaque");
 		three.classList.remove("opaque");
@@ -415,7 +368,7 @@ function clickSeven(elem){
 	} else {}
 }
 function clickEight(elem){
-	if (element2.classList.contains("no-opaque")){
+	if (element2.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.remove("opaque");
 		three.classList.remove("opaque");
@@ -429,7 +382,7 @@ function clickEight(elem){
 	} else {}
 }
 function clickNine(elem){
-	if (element2.classList.contains("no-opaque")){
+	if (element2.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.remove("opaque");
 		three.classList.remove("opaque");
@@ -443,7 +396,7 @@ function clickNine(elem){
 	} else {}
 }
 function clickTen(elem){
-	if (element2.classList.contains("no-opaque")){
+	if (element2.classList.contains("opaque")){
 		one.classList.remove("opaque");
 		two.classList.remove("opaque");
 		three.classList.remove("opaque");
